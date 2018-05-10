3feb96202547c840a634d0b1a7a0f5328a6f56d7
MiguelRavello <errejota7@gmail.com>
2018-05-10 18:48:00 -0500

    seidel

--- seidel/main.cpp
+++ seidel/main.cpp
@@ -7,7 +7,7 @@ int main(){
     vector<double> b = {4,-1,-2};
     vector<double> seed = {0,0,0};
     Seidel<double> S(A,b);
-    S.MetJacobi2(seed,0.00001);
+    S.MetSeidel2(seed,0.00001);
     cout<<S.m_X<<endl;
     return 0;
 }
---
+++ seidel/readme.txt
@@ -0,0 +1,2 @@
+g++ -std=c++11 seidel.h main.cpp -o m
+./m

96c5cf43f49a76ceee2a23d8275f69ac90f4305c
MiguelRavello <errejota7@gmail.com>
2018-05-17 16:59:16 -0400

    plu

--- PLU/main.cpp
+++ PLU/main.cpp
@@ -2,11 +2,12 @@
 //#include"matrix.h"
 
 int main(){
-    vector<vector<double> > matriz = {{ 4, -2, 1},
-                                      {20, -7, 12},
-                                      {-8, 131, 17}};
-    vector<double> b= {11, 70, 17};
-    PLU<double> px(3,3);
+    vector<vector<double> > matriz = {{ -1, 2, -1, 0},
+                                      {2, -1, 1, 1},
+                                      {4, 1, -2, 1},
+                                      {3, 2, -3,4}};
+    vector<double> b= {5, 7, 1, 10};
+    PLU<double> px(4,4);
     vector<double> rpta=px.solucion(matriz,b);
     px.proof();
     cout<<"..............................."<<endl;
